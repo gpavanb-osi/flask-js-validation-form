caf0eeff35e9eb2c8322ceb7c73fef084e95e461
Pavan B G <pgovindaraju@osidigital.com>
2019-07-24 16:33:57 +0530

    tested version

--- server.py
+++ server.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, jsonify
+import sqlite3
 
 app = Flask(__name__, template_folder='app/')
 app.static_folder = 'app/static'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/new_data/', methods=['post'])
+def new_data():
+    userid = request.form.get('userid')
+    password = request.form.get('passid')
+    name = request.form.get('username')
+    address = request.form.get('address')
+    country = request.form.get('country')
+    zipcode = request.form.get('zip')
+    email = request.form.get('email')
+    sex = request.form.get('sex')
+    language = request.form.get('lang')
+    about = request.form.get('desc')
+
+    insert_stmt = (
+        "INSERT INTO students (userid, password, "
+        "name, address, country, "
+        "zipcode, email, sex, language, about) "
+        "VALUES (?, ?, ?, ?, "
+        "?, ?, ?, ?, ?, ?)"
+    )
+    data = (userid, password,
+            name, address, country,
+            zipcode, email, sex,
+            language, about)
+
+    conn = sqlite3.connect('data/database.db')
+    cur = conn.cursor()
+
+    cur.execute(insert_stmt, data)
+    conn.commit()
+    print("Did we do it?")
+    return jsonify(data)
+
+
 app.run(debug=True)
+conn.close()
